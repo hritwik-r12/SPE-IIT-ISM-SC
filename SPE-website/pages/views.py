bc93ab36b55ad580b0792ceb1c0958746f8db1cd
omkar8501 <omkar6142@gmail.com>
2020-09-04 20:56:37 +0530

    a little bit of code added

--- SPE-website/pages/views.py
+++ SPE-website/pages/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello SPE!!!")
+    return render(request, 'pages/home.html')
