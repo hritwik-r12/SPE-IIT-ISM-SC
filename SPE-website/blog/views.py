b8b3fd0bb52b3ab034a8ed02faef977dffcc9092
= <=>
2020-09-06 16:00:04 +0700

    Added blog home, configured urls in the base template to point to home and blog pages

--- SPE-website/blog/views.py
+++ SPE-website/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
 # Create your views here.
+posts = [
+    {
+        'author': 'Shirin Kaul',
+        'title': 'BP-1',
+        'content': 'First post content',
+        'date_posted' : '6 Sept 2020'
+    },
+    {
+        'author': 'XYZ ABC',
+        'title': 'BP-2',
+        'content': 'Second post content',
+        'date_posted' : '6 Sept 2020'
+    },
+]
+
+def home(request):
+    context={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
