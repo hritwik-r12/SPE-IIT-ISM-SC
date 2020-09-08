d77e7e2de03516e85a0dd303cfe31409a9278a75
omkar8501 <omkar6142@gmail.com>
2020-09-08 23:22:26 +0530

    reformatted some lines

--- SPE-website/blog/views.py
+++ SPE-website/blog/views.py
@@ -4,14 +4,12 @@ from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-
 class PostListView(ListView):
-    model= Post
-    template_name='blog/home.html'
-    context_object_name='posts'
-    order=['-date_posted']
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    order = ['-date_posted']
+
 
 class PostDetailView(DetailView):
-    model= Post
-
-    
+    model = Post
