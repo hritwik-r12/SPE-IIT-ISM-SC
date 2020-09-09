ba445b550b56d1e2d6512977aa5216d3b9459356
= <=>
2020-09-09 18:27:58 +0700

    Added pagination to blog pages, searching blogs by certain user

--- SPE-website/blog/urls.py
+++ SPE-website/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView, 
+    PostDetailView,
+    UserPostListView,
+)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
--- SPE-website/blog/views.py
+++ SPE-website/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -8,8 +8,23 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    order = ['-date_posted']
+    order_by = ['-date_posted']
+    paginate_by = 2
+    
+    def get_queryset(self):
+        return Post.objects.order_by('-id')
 
 
 class PostDetailView(DetailView):
     model = Post
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-id')
+        
