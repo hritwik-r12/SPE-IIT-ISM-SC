c956656635d297e894dd1f5a7ea19349d46bb14a
= <=>
2020-09-30 16:20:46 +0700

    added past event and upcomming event page

--- SPE-website/events/urls.py
+++ SPE-website/events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import EventListView, EventDetailView
+from .views import EventListView, EventDetailView, PastEventListView
 
 urlpatterns = [
     path('', EventListView.as_view(), name='events-home'),
+    path('past/', PastEventListView.as_view(), name='events-past'),
     path('Event/<int:pk>/', EventDetailView.as_view(), name='events-detail')
 ]
--- SPE-website/events/views.py
+++ SPE-website/events/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Events
 from django.views.generic import ListView, DetailView
+import datetime
 
 
 def home(request):
@@ -17,7 +18,24 @@ class EventListView(ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        return Events.objects.order_by('-id')
+
+        date_1=datetime.date.today()
+        #date_1=date_1 + datetime.timedelta(days=-1)
+        return Events.objects.filter(reg_date__lte=datetime.date(2040,1,1),
+                                     reg_date__gte=date_1).order_by('id')
 
 class EventDetailView(DetailView):
     model = Events
+
+class PastEventListView(ListView):
+    model = Events
+    template_name = 'events/home.html'
+    context_object_name = 'events'
+    order_by = ['-reg_date']
+    paginate_by = 3
+
+    def get_queryset(self):
+        date_1=datetime.date.today()
+        date_1=date_1 + datetime.timedelta(days=-1)
+        return Events.objects.filter(reg_date__lte=date_1,
+                                     reg_date__gte=datetime.date(2019,1,1))
