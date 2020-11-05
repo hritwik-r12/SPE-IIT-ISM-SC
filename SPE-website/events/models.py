a43224656a0672d89f9e4a4926a09cfebbb7196f
= <=>
2020-11-06 02:44:17 +0700

    Fixed registration forms and uploaded blogs 1. Converted member to text field in registration form 2. Added 5 realistic blogs in markdown with images

--- SPE-website/events/models.py
+++ SPE-website/events/models.py
@@ -24,7 +24,7 @@ class Events(models.Model):
 class Registeration(models.Model):
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
     team_name = models.CharField(max_length=300, null=True, blank=True)
-    Member_1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    Member_1 = models.CharField(max_length=300)
     Mobile_number = models.IntegerField()
     email = models.EmailField(max_length=200)
     Member_2 = models.CharField(max_length=300, null=True, blank=True)
@@ -32,14 +32,14 @@ class Registeration(models.Model):
     Member_4 = models.CharField(max_length=300, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.Member_1.username} {self.event.title} regestration'
+        return f'{self.Member_1} {self.event.title} regestration'
 
 
 class SingleRegistration(models.Model):
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
-    Member_1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    Member_1 = models.CharField(max_length=100)
     Mobile_number = models.IntegerField()
     email = models.EmailField(max_length=200)
 
     def __str__(self):
-        return f'{self.Member_1.username} {self.event.title} single registration'
+        return f'{self.Member_1} {self.event.title} single registration'
