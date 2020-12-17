6bf0aac09d202f8c35334d34d8386cbfec0112de
hyperloop11 <shirin.kaul11@gmail.com>
2020-12-17 15:48:25 +0530

    fixed height contact us form

--- SPE-website/pages/forms.py
+++ SPE-website/pages/forms.py
@@ -1,25 +1,11 @@
-# from django import forms
-# from django.contrib.auth.models import User
-# from .models import Profile
-
-
-# class UserUpdateForm(forms.ModelForm):
-#     email = forms.EmailField()
-#     spe_id = forms.IntegerField(required=False)
-#     IIT_ISM_registration_number = forms.CharField(required=False)
-#
-#     class Meta:
-#         model = User
-#         fields = ['email', 'spe_id', 'IIT_ISM_registration_number']
-#
-#
-# class ProfileUpdateForm(forms.ModelForm):
-#     class Meta:
-#         pass
 from django import forms
 from .models import MessagesForUs
 
 class MessagesForUsForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['message'].widget.attrs.update(style='height: 20em')
+
     class Meta:
         model = MessagesForUs
         fields = ('sender', 'sender_email', 'message')
@@ -27,3 +13,4 @@ class MessagesForUsForm(forms.ModelForm):
             'sender': 'Your name',
             'sender_email': 'Your email',
         }
+        
--- SPE-website/user/views.py
+++ SPE-website/user/views.py
@@ -7,20 +7,6 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import UserProfile
-from verify_email.email_handler import send_verification_email
-
-'''def signup(request):
-    if request.method == 'POST':
-        form = UserSignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created successfully for {username}')
-            return redirect('homepage')
-    else:
-        form = UserSignUpForm()
-    context = {'form': form}
-    return render(request, 'user/signup.html', context)'''
 
 
 def logout(request):
