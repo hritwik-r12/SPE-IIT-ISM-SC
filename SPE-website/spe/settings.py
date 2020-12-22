a08648a27034a1f482ccb79f1bbd0fedd15aedf6
hyperloop11 <shirin.kaul11@gmail.com>
2020-12-22 15:51:01 +0530

    Updated static root

--- SPE-website/spe/settings.py
+++ SPE-website/spe/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
