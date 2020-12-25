11903881d976eb2dd9f750ec64c999cf97703ff3
omkar8501 <omkar6142@gmail.com>
2020-12-25 13:57:05 +0530

    made spe id and reg no unique

--- SPE-website/user/models.py
+++ SPE-website/user/models.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='foobar', on_delete=models.CASCADE)
-    institute_registration_number = models.CharField(max_length=20, null=True, blank=True)
-    SPE_ID = models.CharField(max_length=20, null=True, blank=True)
+    institute_registration_number = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    SPE_ID = models.CharField(max_length=20, null=True, blank=True, unique=True)
 
     def __str__(self):
         return f'{self.user} Profile'
