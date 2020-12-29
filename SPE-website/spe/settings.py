84737175e3bda8703881bef91a66232fbe78b366
hyperloop11 <shirin.kaul11@gmail.com>
2020-12-29 18:22:33 +0530

    emplty

--- SPE-website/spe/settings.py
+++ SPE-website/spe/settings.py
@@ -24,13 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'u#!0l9!&g73jd!#-2o-wv11)et-gyb!axj)_5)98k2l8%x28$c'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
-<<<<<<< HEAD
-# SECURITY WARNING: don't run with debug turned on in production!!!
-DEBUG = False
-=======
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
->>>>>>> 24a2df0b9917a87ad805f73f7c30660549aabb65
+DEBUG = False
 
 ALLOWED_HOSTS = ['iitismspe.herokuapp.com', '127.0.0.1','www.iitismspe.org']
 
