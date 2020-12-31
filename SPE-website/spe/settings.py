2cd3bc45cdaa80ce28d8299f5e86f5b2e02bc8b1
shadan-arif <shadanarif333@gmail.com>
2020-12-31 07:32:03 +0530

    Made some changes in the Gallery

--- SPE-website/spe/settings.py
+++ SPE-website/spe/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!!!
-#SECRET_KEY = 'u#!0l9!&g73jd!#-2o-wv11)et-gyb!axj)_5)98k2l8%x28$c' #dev secret key
+# SECRET_KEY = 'u#!0l9!&g73jd!#-2o-wv11)et-gyb!axj)_5)98k2l8%x28$c' #dev secret key
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
