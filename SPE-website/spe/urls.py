1a254f3e84359c1539e7b26dd974357b9f75179c
shadan-arif <shadanarif333@gmail.com>
2021-01-11 09:28:57 +0530

    Changed route of gallery to tgal

--- SPE-website/spe/urls.py
+++ SPE-website/spe/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('password-reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
     path('SignOut/', user_views.logout, name='signout-page'),
     path('Events/', include('events.urls')),
-    path('Gallery/', pages_views.gallery, name='gallery-page'),
+    path('Gallery/', pages_views.tgal, name='gallery-page'),
     path('tgal/', pages_views.tgal, name='tgal-page'),
     path('tgal2/', pages_views.tgal2, name='tgal2-page'),
     path('intern/', pages_views.intern, name='intern-page'),
